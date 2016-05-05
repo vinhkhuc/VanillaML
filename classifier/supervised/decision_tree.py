Bug fixes for wrong weighted probabilities and wrong average dimension.

classifier/supervised/decision_tree.py
@@ -125,15 +125,16 @@ class DecisionTreeClassifier(AbstractClassifier):
         """ Get weighted prediction probability.
 
         Args:
-            y (ndarray): sample classes N x 1
-            w (ndarray): sample weights N x 1
+            y (ndarray): sample classes, shape N.
+            w (ndarray): sample weights, shape N.
 
         Returns:
             ndarray: weighted prediction probability.
         """
 
         # Class distribution
-        y_prob = np.bincount(y, w) / float(len(y))
+        y_weighted_count = np.bincount(y, w)
+        y_prob = y_weighted_count / sum(y_weighted_count)
 
         # Fill zeros for classes that are not included in y
         diff = len(self._classes) - len(y_prob)

classifier/supervised/random_forest.py
@@ -50,11 +50,11 @@ class RandomForestClassifier(AbstractClassifier):
             tree.fit(X, y)
 
     def predict_proba(self, X):
-        # Get probability prediction for each decision tree
+        # Get prediction probability for each decision tree
         y_prob_trees = np.array([tree.predict_proba(X) for tree in self.trees])
 
         print("y_prob_trees = %s" % y_prob_trees)
-        print("y_prob_trees.mean(axis=2) = %s" % y_prob_trees.mean(axis=2))
+        print("y_prob_trees.mean(axis=0)\n%s" % y_prob_trees.mean(axis=0))
 
         # Return probability average
-        return y_prob_trees.mean(axis=2)
+        return y_prob_trees.mean(axis=0)