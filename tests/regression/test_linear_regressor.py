Fixed wrong sign of MSELoss. Misc.

tests/regression/test_linear_regressor.py
@@ -4,6 +4,7 @@ import numpy as np
 from vanilla_ml.regression.decision_tree_regressor import DecisionTreeRegressor
 from vanilla_ml.regression.linear_regression import LinearRegressor
 from vanilla_ml.util import data_io
+from vanilla_ml.util.metrics.rmse import rmse_score
 
 
 class TestLinearRegressor(unittest.TestCase):
@@ -23,7 +24,7 @@ class TestLinearRegressor(unittest.TestCase):
         pred_y = regr.predict(test_X)
         print("pred_y = %s" % regr.predict(test_X))
 
-        rmse = np.sqrt(np.mean(np.square(test_y - pred_y)))
+        rmse = rmse_score(test_y, pred_y)
         print("RMSE = %g" % rmse)
 
         # self.assertLessEqual(rmse, 6)

vanilla_ml/base/neural_network/activators.py
@@ -3,6 +3,24 @@ import numpy as np
 from module import Module
 
 
+class ReLU(Module):
+    """ ReLU module """
+
+    def fprop(self, input_data):
+        self.output = np.multiply(input_data, input_data > 0)
+        return self.output
+
+    def bprop(self, input_data, grad_output):
+        self.grad_input = np.multiply(grad_output, input_data > 0)
+        return self.grad_input
+
+    def update(self, params):
+        pass
+
+    def share(self, m):
+        pass
+
+
 class Sigmoid(Module):
 
     def fprop(self, input_data):
@@ -10,7 +28,8 @@ class Sigmoid(Module):
         return self.output
 
     def bprop(self, input_data, grad_output):
-        return grad_output * self.output * (1. - self.output)
+        return np.multiply(grad_output, np.multiply(self.output, 1. - self.output))
+        # return grad_output * self.output * (1. - self.output)
 
     def update(self, params):
         pass

vanilla_ml/base/neural_network/layers.py
@@ -53,21 +53,24 @@ class Linear(Module):
 
         return self.output
 
+    # TODO: Rename input_data -> prev_layer_output
     def bprop(self, input_data, grad_output):
-        orig_input_data_shape = input_data.shape
-        high_dimension_input = input_data.ndim > 2
+        # orig_input_data_shape = input_data.shape
+        # high_dimension_input = input_data.ndim > 2
+        #
+        # # Reshape input and grad_output
+        # if high_dimension_input:
+        #     input_data  = input_data.reshape(input_data.shape[0], -1)
+        #     grad_output = grad_output.reshape(grad_output.shape[0], -1)
 
-        # Reshape input and grad_output
-        if high_dimension_input:
-            input_data  = input_data.reshape(input_data.shape[0], -1)
-            grad_output = grad_output.reshape(grad_output.shape[0], -1)
+        # self.weight.grad = self.weight.grad + np.dot(input_data.T, grad_output)
+        # self.bias.grad   = self.bias.grad + grad_output.sum(axis=0)
+        self.weight.grad = np.dot(input_data.T, grad_output)
+        self.bias.grad   = grad_output.sum(axis=0)
+        self.grad_input  = np.dot(grad_output, self.weight.D.T)
 
-        self.weight.grad = self.weight.grad + np.dot(grad_output, input_data.T)
-        self.bias.grad   = self.bias.grad + grad_output.sum(axis=1)
-        self.grad_input  = np.dot(self.weight.D.T, grad_output)
-
-        if high_dimension_input:
-            self.grad_input = self.grad_input.reshape(orig_input_data_shape)
+        # if high_dimension_input:
+        #     self.grad_input = self.grad_input.reshape(orig_input_data_shape)
 
         return self.grad_input
 

vanilla_ml/base/neural_network/loss.py
@@ -18,11 +18,20 @@ class Loss(object):
 
 class MSELoss(Loss):
 
+    def __init__(self):
+        self.size_average = True
+
     def fprop(self, input_data, target_data):
-        return rmse_score(target_data, input_data)
+        cost = np.sum(np.square(target_data - input_data))
+        if self.size_average:
+            cost /= target_data.shape[0]
+        return cost
 
     def bprop(self, input_data, target_data):
-        return 2 * (input_data - target_data) / len(input_data)
+        grad_input = 2 * (input_data - target_data)
+        if self.size_average:
+            grad_input /= target_data.shape[0]
+        return grad_input
 
 
 class CrossEntropyLoss(Loss):
@@ -37,7 +46,7 @@ class CrossEntropyLoss(Loss):
         z = target_data
         cost = np.sum(-np.log(input_data[z]))
         if self.size_average:
-            cost /= input_data.shape[1]
+            cost /= input_data.shape[1]  # FIXME: It should be input_data.shape[0]!!!
 
         return cost
 
@@ -53,7 +62,3 @@ class CrossEntropyLoss(Loss):
             grad_input /= input_data.shape[1]
 
         return grad_input
-
-    def get_error(self, input_data, target_data):
-        y = input_data.argmax(axis=0)
-        return np.sum(y != target_data)

vanilla_ml/base/neural_network/weight.py
@@ -10,7 +10,10 @@ class Weight(object):
         """
         self.sz = sz
         self.D = 0.1 * np.random.standard_normal(sz)
+        # self.D = np.ones(sz)   # TODO: Remove this
         self.grad = np.zeros(sz, np.float32)
+
+        # print("D = %s, grad = %s" % (self.D, self.grad))
 
     def update(self, params):
         """
@@ -20,11 +23,14 @@ class Weight(object):
         if max_grad_norm and max_grad_norm > 0:
             grad_norm = np.linalg.norm(self.grad, 2)
             if grad_norm > max_grad_norm:
+                print("Applying grad_norm ...")
                 self.grad = self.grad * max_grad_norm / grad_norm
 
-        self.D -= params['lrate'] * self.grad
+        # Regularization
+        # self.grad += 10 * 2 * self.D
 
-        print("grad_w = %s" % self.grad)
+        self.D -= params['lrate'] * self.grad
+        # print("D = %s, grad = %s" % (self.D, self.grad))
 
         self.grad[:] = 0
 

vanilla_ml/classifier/supervised/neural_network/mlp_classifier.py
@@ -3,28 +3,29 @@ Multi-layer Perceptron (Feed-forward Neural Network)
 """
 import numpy as np
 
-from vanilla_ml.base.neural_network.activators import Sigmoid
+from vanilla_ml.base.neural_network.activators import Sigmoid, Softmax, ReLU
 from vanilla_ml.base.neural_network.containers import Sequential
 from vanilla_ml.base.neural_network.layers import Linear
 from vanilla_ml.base.neural_network.loss import CrossEntropyLoss
 from vanilla_ml.classifier.supervised.abstract_classifier import AbstractClassifier
+from vanilla_ml.util.metrics.accuracy import accuracy_score
 
 
 class MLPClassifier(AbstractClassifier):
 
     def __init__(self, layers, fit_bias=False, learning_rate=1.0,
-                 mini_batch_size=10, max_iterations=50, tol=1e-5, verbose=True, random_state=42):
+                 batch_size=10, n_epochs=50, tol=1e-5, verbose=True, random_state=42):
 
         assert learning_rate > 0, "Learning rate must be positive."
 
-        # TODO: Support fit_bias
+        # TODO: Remove fit_bias since it's already supported in layers.py
         assert not fit_bias, "fit_bias is not supported."
 
         self.layers = layers
         self.fit_bias = fit_bias
         self.lr = learning_rate
-        self.mini_batch_size = mini_batch_size
-        self.max_iterations = max_iterations
+        self.batch_size = batch_size
+        self.n_epochs = n_epochs
         self.tol = tol
         self.verbose = verbose
         self.random_state = random_state
@@ -48,42 +49,45 @@ class MLPClassifier(AbstractClassifier):
         # one_hot_y = misc.one_hot(y, n_classes)
 
         # Model
-        self.model = _build_model(n_features, self.layers, n_classes)
-
-        # Cost
-        loss = CrossEntropyLoss()
-        loss.size_average = False
-        loss.do_softmax_bprop = True
-
-        # For report
-        total_err  = 0.
-        total_cost = 0.
-        total_num  = 0
+        self.model, loss = _build_model(n_features, self.layers, n_classes)
 
         # SGD params
         params = {"lrate": self.lr, "max_grad_norm": 40}
 
-        # Run SGD
         indices = np.arange(n_samples)
-        for it in range(self.max_iterations):
-            if self.verbose and (it + 1) % 10 == 0:
-                print("Iteration %d ..." % (it + 1))
 
-            mini_batch = np.random.choice(indices, size=self.mini_batch_size, replace=False)
-            input_data, target_data = X[mini_batch], y[mini_batch]
+        # Run SGD
+        for epoch in range(self.n_epochs):
+            if self.verbose and (epoch + 1) % 10 == 0:
+                print("Epoch %d ..." % (epoch + 1))
 
-            # Forward propagation
-            out = self.model.fprop(input_data)
-            total_cost += loss.fprop(out, target_data)
-            total_err  += loss.get_error(out, target_data)
-            total_num  += self.mini_batch_size
+            # For report
+            total_err  = 0.
+            total_cost = 0.
+            total_num  = 0
 
-            print("%d | train error: %g" % (total_num + 1, total_err / total_num))
+            for it in range(n_samples / self.batch_size):
 
-            # Backward propagation
-            grad_output = loss.bprop(out, target_data)
-            self.model.bprop(input_data, grad_output)
-            self.model.update(params)
+                batch = np.random.choice(indices, size=self.batch_size, replace=False)
+                input_data, target_data = X[batch], y[batch]
+
+                # Forward propagation
+                out = self.model.fprop(input_data)
+                total_cost += loss.fprop(out, target_data)
+                pred = out.argmax(axis=1)
+                total_err  += accuracy_score(pred, target_data)
+                total_num  += self.batch_size
+
+                # Backward propagation
+                grad_output = loss.bprop(out, target_data)
+                self.model.bprop(input_data, grad_output)
+                self.model.update(params)
+
+            print("\n* Epoch %d" % (epoch + 1))
+            # print("%d | train error: %g" % (total_num + 1, total_err / total_num))
+            print("pred =\n%s" % pred)
+            print("target_data =\n%s" % target_data)
+            print("accuracy = %.2f%%" % (100. * accuracy_score(pred, target_data)))
 
     def predict_proba(self, X):
         return self.model.fprop(X)
@@ -98,9 +102,16 @@ def _build_model(input_size, layer_sizes, output_size):
         else:
             model.add(Linear(layer_sizes[i - 1], layer_sizes[i]))
         model.add(Sigmoid())
+        # model.add(ReLU())
 
     model.add(Linear(layer_sizes[-1], output_size))
-
+    model.add(Softmax())
     model.modules[-1].skip_bprop = True
 
-    return model
+    # Cost
+    loss = CrossEntropyLoss()
+    loss.size_average = False
+    loss.do_softmax_bprop = True
+
+    return model, loss
+

vanilla_ml/util/metrics/rmse.py
@@ -4,5 +4,9 @@ Root mean square error (RMSE)
 import numpy as np
 
 
+def mse_score(true_y, pred_y):
+    return np.mean(np.square(true_y - pred_y))
+
+
 def rmse_score(true_y, pred_y):
-    return np.sqrt(np.mean(np.square(true_y - pred_y)))
+    return np.sqrt(mse_score(true_y, pred_y))