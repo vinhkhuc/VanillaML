fb09cdfbafe16d2e1cdf322bcc54a1a87265e79f
Vinh Khuc <knvinh@gmail.com>
2016-06-28 16:40:08 -0400

    Fixed PCA and its test.

--- tests/classifier/unsupervised/test_pca.py
+++ tests/classifier/unsupervised/test_pca.py
@@ -8,10 +8,23 @@ from vanilla_ml.classifier.unsupervised.pca import PCA
 from vanilla_ml.util import data_io
 
 
+# FIXME: The results are different from sklearn's PCA
 class TestPCA(unittest.TestCase):
 
-    def test_kmeans(self):
+    def test_pca(self):
         X, y = data_io.load_iris()
+        # import pandas as pd
+        # df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+        #                       header=None)
+        # from sklearn.cross_validation import train_test_split
+        # from sklearn.preprocessing import StandardScaler
+        # X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+        # sc = StandardScaler()
+        # X_train_std = sc.fit_transform(X_train)
+        # X_test_std = sc.transform(X_test)
+        # X = X_train_std
+
         print("X's shape = %s" % (X.shape, ))
 
         # pca = PCA(n_components=2)
@@ -29,5 +42,4 @@ class TestPCA(unittest.TestCase):
             class_member_mask = y == k
             xy = pca_X[class_member_mask]
             plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=10)
-
         plt.show()
--- vanilla_ml/classifier/unsupervised/pca.py
+++ vanilla_ml/classifier/unsupervised/pca.py
@@ -16,15 +16,15 @@ class PCA(object):
     def fit(self, X):
         # Apply standard scaler
         scaled_X = StandardScaler().fit_transform(X)
-
-        # Numpy's cov() expects samples to be represented vertically
+        # scaled_X = X - X.mean(axis=0)
         cov_matrix = np.cov(scaled_X.T)
 
         # Compute eigenvectors and eigenvalues
         eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
 
         sorted_idx = np.abs(eigen_values).argsort()[::-1]
-        self.largest_components = eigen_vectors[sorted_idx[:self.n_components]]
+        self.largest_components = eigen_vectors[:, sorted_idx[:self.n_components]].T
+        # print(self.largest_components.T)
 
     def transform(self, X):
         return np.dot(X, self.largest_components.T)
