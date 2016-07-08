Bug fixes for make_blobs and make_moons. Also fixed the wrong order of passed parameters to precision, recall and f1.

tests/unsupervised/test_kmeans.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 from vanilla_ml.unsupervised.kmeans import KMeans
 from vanilla_ml.util import data_io
 from vanilla_ml.util.metrics.cluster_purity import cluster_purity
@@ -8,7 +10,7 @@ from vanilla_ml.util.metrics.cluster_purity import cluster_purity
 class TestKMeans(unittest.TestCase):
 
     def test_kmeans(self):
-        centers = [[1, 1], [-1, -1], [1, -1]]
+        centers = np.array([[1, 1], [-1, -1], [1, -1]])
         n_clusters = len(centers)
         X, y = data_io.get_clustering_data(centers=centers, n_samples=50, cluster_std=0.7)
         print("X's shape = %s, y's shape = %s" % (X.shape, y.shape))

tests/util/test_metric.py
@@ -11,19 +11,19 @@ class TestMetric(unittest.TestCase):
         pred_y = np.array([0, 0, 1, 1])
         true_y = np.array([1, 1, 1, 0])
 
-        precision = f1.precision(pred_y, true_y)
+        precision = f1.precision(true_y, pred_y)
         self.assertTrue(precision == 0.5)
 
     def recall(self):
         pred_y = np.array([0, 0, 1, 1])
         true_y = np.array([1, 1, 1, 0])
 
-        recall = f1.recall(pred_y, true_y)
+        recall = f1.recall(true_y, pred_y)
         assert(recall == 1. / 3)
 
     def test_f1(self):
         pred_y = np.array([0, 0, 1, 1])
         true_y = np.array([1, 1, 1, 0])
 
-        f1_score = f1.f1_score(pred_y, true_y)
+        f1_score = f1.f1_score(true_y, pred_y)
         self.assertTrue(f1_score == 0.4)

vanilla_ml/util/misc.py
@@ -121,7 +121,7 @@ def train_test_split(X, y, test_size=0.25, random_state=42):
 
 
 # Adapted from sklearn's make_moons()
-def make_moons(n_samples, noise=0.3, random_state=42, factor=.8):
+def make_moons(n_samples, noise=0.3, random_state=42):
     """ Generate two moons.
 
     Args:
@@ -133,22 +133,25 @@ def make_moons(n_samples, noise=0.3, random_state=42, factor=.8):
         tuple: a tuple of X and y
 
     """
-    linspace = np.linspace(0, 2 * np.pi, n_samples // 2 + 1)[:-1]
-    outer_circ_x = np.cos(linspace)
-    outer_circ_y = np.sin(linspace)
-    inner_circ_x = outer_circ_x * factor
-    inner_circ_y = outer_circ_y * factor
+    np.random.seed(random_state)
+
+    n_samples_out = n_samples // 2
+    n_samples_in = n_samples - n_samples_out
+
+    outer_circ_x = np.cos(np.linspace(0, np.pi, n_samples_out))
+    outer_circ_y = np.sin(np.linspace(0, np.pi, n_samples_out))
+    inner_circ_x = 1 - np.cos(np.linspace(0, np.pi, n_samples_in))
+    inner_circ_y = 1 - np.sin(np.linspace(0, np.pi, n_samples_in)) - .5
 
     X = np.vstack((np.append(outer_circ_x, inner_circ_x),
                    np.append(outer_circ_y, inner_circ_y))).T
-    y = np.hstack([np.zeros(n_samples // 2, dtype=np.intp),
-                   np.ones(n_samples // 2, dtype=np.intp)])
+    y = np.hstack([np.zeros(n_samples_in, dtype=np.intp),
+                   np.ones(n_samples_out, dtype=np.intp)])
 
     # Add noise
     X += np.random.normal(scale=noise, size=X.shape)
 
     # Shuffle
-    np.random.seed(random_state)
     shuffled_indices = np.random.permutation(n_samples)
     return X[shuffled_indices], y[shuffled_indices]
 
@@ -180,21 +183,20 @@ def make_blobs(n_samples=100, n_features=2, n_centers=3, centers=None, cluster_s
         centers = np.random.uniform(center_low, center_high,
                                     size=(centers, n_features))
     else:
-        n_features = centers.shape[1]
+        n_centers, n_features = centers.shape
 
     # Calculate number of samples per center
-    n_samples_per_center = [int(n_samples // centers)] * centers
-    for i in range(n_samples % centers):
+    n_samples_per_center = [int(n_samples // n_centers)] * n_centers
+    for i in range(n_samples % n_centers):
         n_samples_per_center[i] += 1
 
     # Generate blobs
-    X = []
-    y = []
-    for i, (n, std) in enumerate(zip(n_samples_per_center, cluster_std)):
-        X.append(centers[i] + np.random.normal(scale=std, size=(n, n_features)))
+    X, y = [], []
+    for i, n in enumerate(n_samples_per_center):
+        X.append(centers[i] + np.random.normal(scale=cluster_std, size=(n, n_features)))
         y += [i] * n
 
-    X = np.array(X)
+    X = np.concatenate(X)
     y = np.array(y)
     rand_indices = np.random.permutation(n_samples)
     return X[rand_indices], y[rand_indices]